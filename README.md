bdd67fdea3ade0684a00a56afecdeb142d46507f
Acris Liu <acrisliu@gmail.com>
2019-07-04 23:48:38 +0800

    :harmer: refactor

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# ss-merlin-stats门户
-
-## 快速入门
-
-```
-  // 下载
- npm install
- 
- // 启动项目
- npm run dev
- 
- // 打开 http://localhost:8080
-```
+# ss-merlin-stats-portal
---
+++ js/data.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        totalUser: 0,
+        stats: []
+    },
+    created: function () {
+        this.loadData()
+    },
+    methods: {
+        loadData: function () {
+            axios.get('https://ss-merlin.iloli.li/stats')
+                .then(function (response) {
+                    self.totalUser = response.reduce((a, b) => a + b.count, 0)
+                    self.stats = response
+                })
+                .catch(function (error) {
+                    console.error(error)
+                });
+        }
+    }
+})
